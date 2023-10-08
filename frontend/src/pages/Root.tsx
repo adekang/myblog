d4f34a5b182b8dc8b5872af09f084bebf931ca23
adekang <adekang@163.com>
2023-10-08 13:56:43 +0800

    feat: 前端 首页样式

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { Card } from 'antd'
+import { AEntryItem } from '@/components/AEntryItem'
 
 interface Props {
 }
 
 export const Root: React.FC<Props> = () => {
   return (
-        <>
-            <h1>Root</h1>
-        </>
+        <div className="w-full flex flex-row gap-4">
+            <Card className="basis-1/12 h-fit">
+                tags
+            </Card>
+            <Card className='basis-9/12 truncate min-h-80vh divide-y divide-solid'>
+                <AEntryItem/>
+                <AEntryItem/>
+                <AEntryItem/>
+                <AEntryItem/>
+                <AEntryItem/>
+                <AEntryItem/>
+            </Card>
+            <Card className='basis-2/12 h-fit '>
+                adekang
+            </Card>
+        </div>
   )
 }
