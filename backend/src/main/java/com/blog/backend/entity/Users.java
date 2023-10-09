e99ca6ac6e408bc3160465c53b66f7c1e970fd33
adekang <adekang@163.com>
2023-10-09 19:35:12 +0800

    feat: 实体类创建

--- backend/src/main/java/com/blog/backend/entity/Users.java
+++ backend/src/main/java/com/blog/backend/entity/Users.java
@@ -10,6 +10,7 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * @author adekang
@@ -22,36 +23,26 @@ public class Users implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty("用户ID")
     @TableId(value = "user_Id", type = IdType.AUTO)
     private Long userId;
 
-    @ApiModelProperty("用户名")
     private String userName;
 
-    @ApiModelProperty("用户昵称")
     private String userNickname;
 
-    @ApiModelProperty("用户密码")
     private String pwd;
 
-    @ApiModelProperty("用户邮箱")
     private String email;
 
-    @ApiModelProperty("用户头像")
     private String avatar;
 
-    @ApiModelProperty("注册时间")
-    private LocalDateTime createTime;
+    private Date createTime;
 
-    @ApiModelProperty("用户生日")
-    private LocalDate birthday;
+    private Date birthday;
 
-    @ApiModelProperty("用户年龄")
     private Integer age;
 
-    @ApiModelProperty("用户手机号")
-    private String moblePhone;
+    private String mobilePhone;
 
 
 }
